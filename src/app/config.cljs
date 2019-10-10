22fac2a7bc270654761872f14a16400e1b15b05c
jiyinyiyong <jiyinyiyong@gmail.com>
2019-10-11 02:33:55 +0800

    upgrade deps; fix CDN url

--- src/app/config.cljs
+++ src/app/config.cljs
@@ -17,7 +17,7 @@
 (def site
   {:dev-ui "http://localhost:8100/main-fonts.css",
    :release-ui "http://cdn.tiye.me/favored-fonts/main-fonts.css",
-   :cdn-url "http://cdn.tiye.me/calcit-workflow/",
+   :cdn-url "http://cdn.tiye.me/edn-grid/",
    :title "Calcit",
    :icon "http://cdn.tiye.me/logo/mvc-works.png",
    :storage-key "workflow"})
